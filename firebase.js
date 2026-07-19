d6502ad85de7d5f8b9a37f845b39d67b92d59c22
سبحان <sobhan2399@gmail.com>
2026-07-19 17:56:35 +0330

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 // اتصال Firebase سالن معجزه
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCeDSU7d3JyNgny0MWjEpYYY4QrMTMDngs",
+  authDomain: "salon-mojezeh.firebaseapp.com",
+  projectId: "salon-mojezeh",
+  storageBucket: "salon-mojezeh.firebasestorage.app",
+  messagingSenderId: "1023073696440",
+  appId: "1:1023073696440:web:98f95b75e36408d990124d"
 };
 
 console.log("Firebase آماده اتصال است");
