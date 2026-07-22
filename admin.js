ddba62c614ca0c6867af3a58ec4566938f0b28d5
سبحان <sobhan2399@gmail.com>
2026-07-22 15:16:14 +0330

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,90 @@
+import { db } from "./firebase.js";
 
+import {
+
+collection,
+getDocs,
+updateDoc,
+doc
+
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const tbody=document.getElementById("todayBookings");
+
+const todayCount=document.getElementById("todayCount");
+
+const monthCount=document.getElementById("monthCount");
+
+const customerCount=document.getElementById("customerCount");
+
+async function loadBookings(){
+
+const snapshot=await getDocs(collection(db,"bookings"));
+
+tbody.innerHTML="";
+
+let today=0;
+
+let month=0;
+
+snapshot.forEach((booking)=>{
+
+const data=booking.data();
+
+const tr=document.createElement("tr");
+
+tr.innerHTML=`
+
+<td>${data.name}</td>
+
+<td>${data.phone}</td>
+
+<td>${data.date}</td>
+
+<td>${data.time}</td>
+
+<td>
+
+<button class="done"
+
+onclick="finishBooking('${booking.id}')">
+
+انجام شد
+
+</button>
+
+</td>
+
+`;
+
+tbody.appendChild(tr);
+
+today++;
+
+month++;
+
+});
+
+todayCount.innerText=today;
+
+monthCount.innerText=month;
+
+customerCount.innerText=snapshot.size;
+
+}
+
+window.finishBooking=async(id)=>{
+
+await updateDoc(doc(db,"bookings",id),{
+
+status:"done"
+
+});
+
+alert("رزرو انجام شد");
+
+loadBookings();
+
+}
+
+loadBookings();
